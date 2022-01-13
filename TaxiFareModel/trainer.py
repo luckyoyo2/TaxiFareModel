597bc98953a51c2a503b0ef8499653c3e86d30bd
Yoann Diep <yoann.diep@hotmail.fr>
2022-01-13 19:34:21 +0100

    Added the mlflow tracking

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -7,10 +7,17 @@ from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import numpy as np
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
+
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self,
+                 X,
+                 y,
+                 mlflow_uri="https://mlflow.lewagon.co/",
+                 experiment_name="[FR] [Paris] [luckyoyo2] TaxiFareModel v1"):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -18,6 +25,9 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.mlflow_uri = mlflow_uri
+        self.experiment_name = experiment_name
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -55,6 +65,29 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         return rmse
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.mlflow_uri)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 
 if __name__ == "__main__":
@@ -71,4 +104,10 @@ if __name__ == "__main__":
     trainer = Trainer(X_train, y_train)
     trainer.run()
     # evaluate
-    print(trainer.evaluate(X_test, y_test))
+    results = trainer.evaluate(X_test, y_test)
+
+    trainer.mlflow_log_param("model", "Linear Regression")
+    trainer.mlflow_log_metric("rmse", results)
+
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
